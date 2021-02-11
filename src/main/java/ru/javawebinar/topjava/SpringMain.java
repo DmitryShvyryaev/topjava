53d1ac2402b078cd3a42c94134ddaaa5af2bff98
DmitryShvyryaev <DmitryShvuryaev@yandex.ru>
2021-02-11 20:10:59 +0300

    impl_user_repository

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -4,6 +4,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
@@ -13,8 +14,22 @@ public class SpringMain {
         // java 7 automatic resource management (ARM)
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+//            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+//            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+
+            //test
+            UserRepository repository = appCtx.getBean(UserRepository.class);
+
+            System.out.println("");
+            repository.getAll().forEach(System.out::println);
+            System.out.println("");
+            User user = repository.getByEmail("dmitry@mail.ru");
+            System.out.println(user);
+            user.setName("Grisha");
+            System.out.println(repository.save(user));
+            System.out.println(repository.get(2));
+            System.out.println(repository.delete(2));
+            repository.getAll().forEach(System.out::println);
         }
     }
 }
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,43 +3,68 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.AbstractNamedEntity;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
+
+    {
+        save(new User(1, "Taisia", "taya@mail.ru", "111", Role.USER));
+        save(new User(2, "Dmitry", "dmitry@mail.ru", "111", Role.ADMIN, Role.USER));
+        save(new User(3, "Alexy", "alexy@mail.ru", "111", Role.USER));
+        save(new User(4, "Michail", "mich@mail.ru", "111", Role.USER));
+        save(new User(5, "Johanson", "jaoh@mail.ru", "111", Role.USER));
+    }
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return repository.remove(id) != null;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        if (user.isNew()) {
+            user.setId(SecurityUtil.authUserId());
+            user.setCaloriesPerDay(SecurityUtil.authUserCaloriesPerDay());
+            repository.put(user.getId(), user);
+            return user;
+        }
+        return repository.put(user.getId(), user);
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        List<User> users = new ArrayList<>(repository.values());
+        //sorting
+        users.sort(Comparator.comparing(AbstractNamedEntity::getName));
+
+        return users;
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+        return repository.values().stream().filter(user -> user.getEmail().equals(email)).findFirst().get();
     }
 }
