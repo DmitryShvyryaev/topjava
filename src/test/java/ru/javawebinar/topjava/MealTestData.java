c66f4b0acf7ca52aa10f53a9af7ff90713f572d2
DmitryShvyryaev <DmitryShvuryaev@yandex.ru>
2021-02-22 18:03:59 +0300

    add_meal_service_test

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -1,12 +1,43 @@
 package ru.javawebinar.topjava;
 
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 public class MealTestData {
 
+    public static final int USER_ID = START_SEQ;
+    public static final int ALIEN_ID = START_SEQ + 1;
+    private static int START_MEAL_ID = START_SEQ + 1;
+
+    public static final int NOT_FOUND_ID = START_SEQ + 150;
+
+    public static final LocalDate START_DATE_TEST = LocalDate.of(2020, Month.JANUARY, 31);
+    public static final LocalDate END_DATE_TEST = LocalDate.of(2020, Month.JANUARY, 30);
+
+    public static final List<Meal> TEST_MEALS = MealsUtil.meals.stream().peek(meal -> meal.setId(++START_MEAL_ID)).collect(Collectors.toList());
+
+    static {
+        TEST_MEALS.sort(Comparator.comparing(Meal::getDateTime).reversed());
+    }
+
     public static Meal getNew() {
         return new Meal(null, LocalDateTime.now(), "testDesc", 1250);
+    }
+
+    public static Meal getUpdated() {
+        Meal meal = new Meal(TEST_MEALS.get(6));
+        meal.setDateTime(LocalDateTime.of(2020, Month.MARCH, 15, 12, 34, 0));
+        meal.setCalories(666);
+        meal.setDescription("UniqueDescription");
+        return meal;
     }
 }
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -11,14 +11,18 @@ import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.DateTimeUtil;
+import ru.javawebinar.topjava.util.Util;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.MealTestData.*;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
@@ -33,64 +37,114 @@ public class MealServiceTest {
 
     @Test
     public void get() {
-        Meal meal = mealService.get(100003, 100000);
-        System.out.println(meal);
+        Meal expected = TEST_MEALS.get(4);
+        Meal actual = mealService.get(expected.getId(), USER_ID);
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
     }
 
     @Test
-    public void getIncorrectId() {
-        Assert.assertThrows(NotFoundException.class, () -> mealService.get(5, 100000));
+    public void getNotExist() {
+        Assert.assertThrows(NotFoundException.class, () -> mealService.get(NOT_FOUND_ID, USER_ID));
     }
 
     @Test
-    public void getIncorrectUserId() {
-        Assert.assertThrows(NotFoundException.class, () -> mealService.get(100003, 200000));
+    public void getWithIncorrectUserId() {
+        Meal expected = TEST_MEALS.get(4);
+        Assert.assertThrows(NotFoundException.class, () -> mealService.get(expected.getId(), 9));
     }
 
     @Test
     public void delete() {
-        mealService.delete(100003, 100000);
-        Assert.assertThrows(NotFoundException.class, ()->mealService.get(100003, 100000));
+        int id = TEST_MEALS.get(5).getId();
+        mealService.delete(id, USER_ID);
+        Assert.assertThrows(NotFoundException.class, () -> mealService.get(id, USER_ID));
     }
 
     @Test
     public void deleteNotFound() {
-        Assert.assertThrows(NotFoundException.class, ()->mealService.get(16, 100000));
+        Assert.assertThrows(NotFoundException.class, () -> mealService.get(NOT_FOUND_ID, USER_ID));
     }
 
     @Test
     public void deleteIncorrectUserId() {
-        Assert.assertThrows(NotFoundException.class, ()->mealService.get(100003, 100001));
+        int id = TEST_MEALS.get(3).getId();
+        Assert.assertThrows(NotFoundException.class, () -> mealService.get(id, 999));
     }
 
     @Test
-    public void getBetweenInclusive() {
-        mealService.getBetweenInclusive(LocalDate.of(2020, Month.JANUARY, 31),
-                LocalDate.of(2020, Month.FEBRUARY, 1), 100000).forEach(System.out::println);
+    public void getBetweenInclusiveForInterSec() {
+        List<Meal> meals = mealService.getBetweenInclusive(START_DATE_TEST, END_DATE_TEST, USER_ID);
+        List<Meal> expected = TEST_MEALS.stream().filter(meal -> Util.isBetweenHalfOpen(meal.getDateTime(),
+                DateTimeUtil.atStartOfDayOrMin(START_DATE_TEST), DateTimeUtil.atStartOfNextDayOrMax(END_DATE_TEST))).collect(Collectors.toList());
+        assertThat(meals).usingRecursiveFieldByFieldElementComparator().isEqualTo(expected);
+    }
+
+    @Test
+    public void getBetweenInclusiveForStartDate() {
+        List<Meal> meals = mealService.getBetweenInclusive(null, END_DATE_TEST, USER_ID);
+        List<Meal> expected = TEST_MEALS.stream().filter(meal -> Util.isBetweenHalfOpen(meal.getDateTime(),
+                null, DateTimeUtil.atStartOfNextDayOrMax(END_DATE_TEST))).collect(Collectors.toList());
+        assertThat(meals).usingRecursiveFieldByFieldElementComparator().isEqualTo(expected);
+    }
+
+    @Test
+    public void getBetweenInclusiveForEndDate() {
+        List<Meal> meals = mealService.getBetweenInclusive(START_DATE_TEST, null, USER_ID);
+        List<Meal> expected = TEST_MEALS.stream().filter(meal -> Util.isBetweenHalfOpen(meal.getDateTime(),
+                DateTimeUtil.atStartOfDayOrMin(START_DATE_TEST), null)).collect(Collectors.toList());
+        assertThat(meals).usingRecursiveFieldByFieldElementComparator().isEqualTo(expected);
+    }
+
+    @Test
+    public void getBetweenInclusiveForNulls() {
+        List<Meal> meals = mealService.getBetweenInclusive(null, null, USER_ID);
+        assertThat(meals).usingRecursiveFieldByFieldElementComparator().isEqualTo(TEST_MEALS);
     }
 
     @Test
     public void getAll() {
-        mealService.getAll(100000).forEach(System.out::println);
+        List<Meal> meals = mealService.getAll(USER_ID);
+        assertThat(meals).usingRecursiveFieldByFieldElementComparator().isEqualTo(TEST_MEALS);
+    }
+
+    @Test
+    public void getAllWithoutMeals() {
+        List<Meal> meals = mealService.getAll(ALIEN_ID);
+        assertThat(meals).usingRecursiveFieldByFieldElementComparator().isEqualTo(Collections.EMPTY_LIST);
     }
 
     @Test
     public void update() {
+        Meal updated = getUpdated();
+        mealService.update(updated, USER_ID);
+        assertThat(mealService.get(updated.getId(), USER_ID)).usingRecursiveComparison().isEqualTo(getUpdated());
+    }
 
+    @Test
+    public void updateNotMineMeal() {
+        Assert.assertThrows(NotFoundException.class, () -> mealService.update(getUpdated(), NOT_FOUND_ID));
+    }
+
+    @Test
+    public void updateNotExist() {
+        Meal updated = getUpdated();
+        updated.setId(150);
+        Assert.assertThrows(NotFoundException.class, () -> mealService.update(updated, USER_ID));
     }
 
     @Test
     public void create() {
-        Meal created = mealService.create(MealTestData.getNew(), 100000);
-        Meal copy = new Meal(created);
-        assertThat(created).usingRecursiveComparison().isEqualTo(copy);
-        assertThat(copy).usingRecursiveComparison().isEqualTo(mealService.get(created.getId(), 100000));
+        Meal created = mealService.create(getNew(), USER_ID);
+        Meal newMeal = getNew();
+        newMeal.setId(created.getId());
+        assertThat(created).usingRecursiveComparison().ignoringFields("dateTime").isEqualTo(newMeal);
+        assertThat(mealService.get(newMeal.getId(), USER_ID)).usingRecursiveComparison().ignoringFields("dateTime").isEqualTo(newMeal);
     }
 
     @Test
     public void createDublicate() {
         Assert.assertThrows(DataAccessException.class, () ->
                 mealService.create(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0),
-                        "Завтрак", 500), 100000));
+                        "Завтрак", 500), USER_ID));
     }
 }
