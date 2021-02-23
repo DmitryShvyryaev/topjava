407b1e79639ef452d49f50b9caf72098524c0997
DmitryShvyryaev <DmitryShvuryaev@yandex.ru>
2021-02-23 12:58:14 +0300

    fix_bug_test

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
@@ -1,10 +1,12 @@
 package ru.javawebinar.topjava.web.user;
 
 import org.junit.*;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
@@ -39,14 +41,14 @@ public class InMemoryAdminRestControllerTest {
         repository.init();
     }
 
-//    @Test
-//    public void delete() {
-//        controller.delete(USER_ID);
-//        Assert.assertNull(repository.get(USER_ID));
-//    }
-//
-//    @Test
-//    public void deleteNotFound() {
-//        Assert.assertThrows(NotFoundException.class, () -> controller.delete(NOT_FOUND));
-//    }
+    @Test
+    public void delete() {
+        controller.delete(USER_ID);
+        Assert.assertNull(repository.get(USER_ID));
+    }
+
+    @Test
+    public void deleteNotFound() {
+        Assert.assertThrows(NotFoundException.class, () -> controller.delete(NOT_FOUND));
+    }
 }
