c9083c2191a8eb1426e9cf702a023439148ca5a2
DmitryShvyryaev <DmitryShvuryaev@yandex.ru>
2021-04-06 23:22:26 +0300

    add_meal_ui_controller

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUiController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUiController.java
@@ -1,56 +1,55 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Objects;
 
 import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
 import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
-@Controller
-@RequestMapping("/meals")
-public class JspMealController extends AbstractMealController {
+@RestController
+@RequestMapping(value = "profile/meals", produces = MediaType.APPLICATION_JSON_VALUE)
+public class MealUiController extends AbstractMealController {
 
-    @GetMapping("/delete")
-    public String delete(HttpServletRequest request) {
-        super.delete(getId(request));
-        return "redirect:/meals";
+    @Override
+    @GetMapping
+    public List<MealTo> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void createWithNoContent(@RequestParam LocalDateTime dateTime,
+                                    @RequestParam String description,
+                                    @RequestParam int calories) {
+        super.create(new Meal(dateTime, description, calories));
     }
 
     @GetMapping("/update")
     public String update(HttpServletRequest request, Model model) {
         model.addAttribute("meal", super.get(getId(request)));
         return "mealForm";
-    }
-
-    @GetMapping("/create")
-    public String create(Model model) {
-        model.addAttribute("meal", new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "", 1000));
-        return "mealForm";
-    }
-
-    @PostMapping
-    public String updateOrCreate(HttpServletRequest request) {
-        Meal meal = new Meal(LocalDateTime.parse(request.getParameter("dateTime")),
-                request.getParameter("description"),
-                Integer.parseInt(request.getParameter("calories")));
-
-        if (request.getParameter("id").isEmpty()) {
-            super.create(meal);
-        } else {
-            super.update(meal, getId(request));
-        }
-        return "redirect:/meals";
     }
 
     @GetMapping("/filter")
